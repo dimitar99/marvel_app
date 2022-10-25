890b228d829df353039aea03ebbbb43a1a8b7a71
dimitar <dimitar@rudo.es>
2022-10-25 12:24:05 +0200

    Initial commit

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return SafeArea(
+      child: Scaffold(
+        body: Center(child: Text('Hola mundo'),),
+      ),
+    );
+  }
+
+}
